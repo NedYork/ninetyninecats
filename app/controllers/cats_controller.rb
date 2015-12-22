73b8e18ac511143af575b5644ab7b1f06c322d1d
John&Edward <J.E@appacademy.io>
2015-12-22 12:14:34 -0500

    Add index and show actions for Cat.

---
+++ app/controllers/cats_controller.rb
@@ -0,0 +1,16 @@
+class CatsController < ApplicationController
+
+
+  def index
+    @cats = Cat.all
+    render :index
+  end
+
+  def show
+    @cat = Cat.find(params[:id])
+    render :show
+  end
+
+  
+
+end
